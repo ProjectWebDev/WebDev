50e584850aaf2a36e7c9142fbc0a46c40dd4557d
Carlo <carlodamico137@gmail.com>
2021-03-28 13:15:09 +0200

    cover

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ function ChangeColor(){
     let moon = document.getElementById("moon");
     let circle = document.getElementById('circle');
     let circleBox = document.getElementById("circle-box");
-    if (moon.style.display == "none"){
-        moon.style.display = "block";
-        sun.style.display = "none";
-        circle.style = []
-        circle.style.right = "0px";
-        body.style.backgroundColor = "#000"
-        circle.style.backgroundColor = "#000";
-        circleBox.style.backgroundColor = "#fff"
+    let card = document.getElementById('card');
+    let contenuto = document.getElementById('contenuto');
+    let title = document.getElementById('title');
+
+    const cardColor = () =>{
+        if ((moon.style.display == "none") || (moon.style.display == "")){
+            card.style.backgroundColor = '#000';
+            title.style.color ='#fff';
+            contenuto.style.color ='#fff';
+            card.classList.add("act");
+            
+        }else {
+            card.style.backgroundColor = '#fff';
+            title.style.color ='#000';
+            contenuto.style.color ='#000';
+            card.style.transform = 'rotateY(360deg), translateY(30%)';
+        }
     }
-    else if (moon.style.display == ""){
+
+    cardColor()
+
+    if (moon.style.display == "none" || moon.style.display == ""){
         moon.style.display = "block";
         sun.style.display = "none";
         circle.style = []
         circle.style.right = "0px";
         body.style.backgroundColor = "#000"
         circle.style.backgroundColor = "#000";
-        circleBox.style.backgroundColor = "#fff"
+        circleBox.style.backgroundColor = "#fff";
+        
     }
     else{
         sun.style.display = "block";
